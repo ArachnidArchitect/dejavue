2a75a9e785116922762a1c0572a0fcb338086e92
ArachnidArchitect <proffesionalworkingstuff@gmail.com>
2024-06-24 14:24:04 +0200

    added all the components, views and a navigation bar

---
+++ dejavue/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
+// https://arachnidarchitect.github.io/-06.24.24-JsonData/
---
+++ dejavue/src/views/FriendsView.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        this is the friends view
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
